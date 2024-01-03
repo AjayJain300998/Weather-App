4dfcd8cd9a29dc439d60db7d259e45b42092d19b
AjayJain300998 <ajayjaincsn@gmail.com>
2024-01-03 14:10:24 +0530

    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let apiKey = "f0c81e2787ad630b98c470df61165ef3";
+let apiKey = "aff976b7ace309a80f9081abf9096421";
 let apiUrl = "https://api.openweathermap.org/data/2.5/weather?";
 let city;
 let userCity = document.getElementById("userText");
@@ -8,51 +8,50 @@ let humidity = document.getElementById("humditiy_info");
 let windInfo = document.getElementById("wind_info");
 let weatherImage = document.querySelector(".weather_img");
 
-// async function getweatherData(){
+// async function getWeatherData(){
 //     city = userCity.value;
 //     console.log(city);
 //     let response = await fetch(
-//       `https://https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+//       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
 //     );
 //     let data = await response.json();
 //     console.log(data);
 //     ct.innerHTML = data.name;
 //     temperature.innerHTML = Math.round(data.main.temp - 273.15)+"°C";
-//     humidity.innerHTML = data.main.humidity + "%";
-//     windInfo.innerHTML = data.wind.speed+"km/h";
+//     humiditiy_info.innerHTML = data.main.humidity + "%";
+//     wind_info.innerHTML = data.wind.speed+"km/h";
 // }
 
 
 
-async function getweatherData() {
-    if (userCity.value == ""); {
-        alert("ENETR A CITY NAME");
-    } 
-     else {
+async function getWeatherData() {
+    if (userCity.value == "") {
+        alert("ENTER A CITY NAME");
+    }else {
         city = userCity.value;
         let response = await fetch(
-            `https://https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
         );
 
         let data = await response.json();
         ct.innerHTML = data.name;
-        temperature.innerHTML = Math.round(data.main.temp - 273.15) + "°C";s
-        humidity.innerHTML = data.main.humidity + "%";
-        windInfo.innerHTML = data.wind.speed + "km/h";
+        temperature.innerHTML = Math.round(data.main.temp - 273.15) + "°C";
+        humiditiy_info.innerHTML = data.main.humidity + "%";
+        wind_info.innerHTML = data.wind.speed + "km/h";
         userCity.value = "";
-        // if (data.weather[0].main == "Clear") {
-        //     weatherImage.src = "Clear.png";
-        // } else if (data.weather[0].main == "Snow") {
-        //     weatherImage.src = "Snow.png";
-        // } else if (data.weather[0].main == "Clouds") {
-        //     weatherImage.src = "Clouds.png";
-        // } else if (data.weather[0].main == "Drizzle") {
-        //     weatherImage.src = "Drizzle.png";
-        // } else if (data.weather[0].main == "Mist") {
-        //     weatherImage.src = "Mist.png";
-        // } else if (data.weather[0].main == "Rain") {
-        //     weatherImage.src = "Rain.png";
-        // }
+        if (data.weather[0].main == "Clear") {
+            weatherImage.src = "4102326_cloud_sun_sunny_weather_icon.png";
+        } else if (data.weather[0].main == "Snow") {
+            weatherImage.src = "snow.png";
+        } else if (data.weather[0].main == "Clouds") {
+            weatherImage.src = "clouds.png";
+        } else if (data.weather[0].main == "Drizzle") {
+            weatherImage.src = "Drizzle.png";
+        } else if (data.weather[0].main == "Mist") {
+            weatherImage.src = "Mist.png";
+        } else if (data.weather[0].main == "Rain") {
+            weatherImage.src = "Rain.png";
+        }
         console.log(data);
     }
 
